6afca15c4fcbc05f30cdb2c081529720cc51dadf
sushma shaw <sushmashaw801@gmail.com>
2024-12-31 23:36:35 +0530

    added packages related to testing components

--- Front-end/vite.config.ts
+++ Front-end/vite.config.ts
@@ -8,4 +8,24 @@ export default defineConfig({
     port: 3000,
     
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './setup.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'lcov', 'json-summary'],
+      reportsDirectory: './coverage',
+      exclude: ['src/context', 'src/zustant'],
+      include: ['src/**'],
+    },
+    include: ['**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/cypress/**',
+      '**/.{idea,git,cache,output,temp}/**',
+      '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build,eslint,prettier}.config.*',
+    ],
+  },
 })
